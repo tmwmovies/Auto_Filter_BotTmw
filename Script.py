9f947575774b7789d11a917f48db54ec92f7f4ab
tmwmovies <tmwmovies94@gmail.com>
2025-10-20 20:20:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/tmw_channels">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/tmw_channels">[ TMW_Channels ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
